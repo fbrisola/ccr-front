dd71f10c100fdc8d7c6423fd678d07ddf9a3f9d2
Fernando Brisola Batista <Fernando Brisola Batista>
2017-01-19 03:19:47 -0200

    Membros e Endereços nas respectivas views

--- src/app/addresses.component.ts
+++ src/app/addresses.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'address',
-    template: '',
+    selector: 'addresses',
+    templateUrl: './addresses.component.html',
 })
 
 export class AddressesComponent {
-    
+    title = 'Endereços';
 }
