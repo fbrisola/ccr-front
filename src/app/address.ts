d038a5a75bb2b9458e51b1168c2d1cf9e23f39f2
Fernando Brisola Batista <Fernando Brisola Batista>
2017-01-19 00:26:23 -0200

    creates address and person classes

---
+++ src/app/address.ts
@@ -0,0 +1,9 @@
+export class Address {
+    id: number;
+    zipcode: string;
+    street: string;
+    district: string;
+    city: string;
+    state: string;
+    phone: string;
+}
---
+++ src/app/person.ts
@@ -0,0 +1,11 @@
+import {Address} from './Address';
+
+export class Person {
+    id: number;
+    name: string;
+    lastname: string;
+    birthdate: string;
+    cel: string;
+    email: string;
+    address: Address;
+}
