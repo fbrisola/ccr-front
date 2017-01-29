1d7397b3a743dd71b048646c17076c77a1aa5ad4
Fernando Brisola Batista <Fernando Brisola Batista>
2017-01-28 23:57:33 -0200

    peopleCount in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Person } from './person';
+import { PersonService } from './person.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [PersonService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'CCR Admin v0.0';
+  people: Person[];
+  peopleCount: number;
+
+  constructor(
+    private personService: PersonService
+  ) { }
+
+  ngOnInit(): void {
+    this.personService.getPeople()
+    .then(people => this.peopleCount = people.length)
+  }
 }
