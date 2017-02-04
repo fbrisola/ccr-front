846433a2824ecb5137a6e532450e8baf0a42c2b6
Fernando Brisola Batista <Fernando Brisola Batista>
2017-02-04 15:07:16 -0200

    uses in memory data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AddressesComponent } from './addresses/addresses.component';
@@ -26,6 +30,7 @@ import { PersonService } from './person.service';
     BrowserModule,
     FormsModule,
     HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule,
   ],
   providers: [PersonService],
--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -1,21 +1,38 @@
 import { Injectable } from '@angular/core';
-
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 import { Person } from './person';
 import { PEOPLE } from './mock-people';
 
 @Injectable()
 export class PersonService {
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  private peopleUrl = 'api/people';  // URL to web api
 
-  constructor() { }
+  constructor(
+    private http: Http
+  ) { }
 
   getPeople(): Promise<Person[]> {
-    return Promise.resolve(PEOPLE);
+    return this.http.get(this.peopleUrl)
+      .toPromise()
+      .then(response => response.json().data as Person[])
+      .catch(this.handleError);
   }
 
   getPerson(id: number): Promise<Person> {
-    return this.getPeople()
-      .then(people => people.find(person => person.id === id));
+    const url = `${this.peopleUrl}/${id}`;
+    return this.http.get(url)
+      .toPromise()
+      .then(response => response.json().data as Person)
+      .catch(this.handleError);
   }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
 
 }
