b5c68f4bc9d3a7050e66fb00b223bf6edc27843f
Fernando Brisola Batista <Fernando Brisola Batista>
2017-02-06 14:55:29 -0200

    adds update method (real data)

--- src/app/person-edit/person-edit.component.ts
+++ src/app/person-edit/person-edit.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Location } from '@angular/common';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/operator/finally';
+import 'rxjs/add/operator/finally';
 
 import { PeopleComponent } from '../people/people.component';
 import { Person } from '../person';
@@ -22,7 +24,8 @@ export class PersonEditComponent implements OnInit {
   constructor(
     private personService: PersonService,
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private router: Router
   ) { }
 
   ngOnInit(): void {
@@ -33,6 +36,12 @@ export class PersonEditComponent implements OnInit {
     this.route.params
       .switchMap((params: Params) => this.personService.getPerson(+params['id']))
       .subscribe(person => this.person = person);
+  }
+
+  save(): void {
+    this.personService.updatePerson(this.person)
+    .subscribe(person => this.person = person);
+    this.router.navigateByUrl('people');
   }
 
   goBack(): void {
--- src/app/person.service.ts
+++ src/app/person.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response } from '@angular/http';
+import { Http, Headers, Response, RequestOptions } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
 // Obbservables
@@ -38,5 +38,15 @@ export class PersonService {
       .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
 
   }
+
+// Update a person
+    updatePerson (body: Person): Observable<Person> {
+        let bodyString = JSON.stringify(body); // Stringify payload
+        let headers      = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+        let options       = new RequestOptions({ headers: headers }); // Create a request option
+        return this.http.put(`${this.peopleUrl}/${body['id']}`, body, options) // ...using put request
+                         .map((res:Response) => res.json()) // ...and calling .json() on the response to return data
+                         .catch((error:any) => Observable.throw(error.json().error || 'Server error')); //...errors if any
+    }   
 
 }
